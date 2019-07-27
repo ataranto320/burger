3ad9efaecbdf9ba3d7089630632b48a6621ca503
Austin T <austin.taranto@gmail.com>
2019-07-26 21:27:14 -0400

    slight outline of seeds sql insert values

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Mexi", "");
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("BBQ", "");
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Apple Bacon", "");
