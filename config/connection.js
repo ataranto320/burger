01a34b0986f298eb3cbebd4d549c7ced9c0db23b
Austin T <austin.taranto@gmail.com>
2019-07-28 16:21:42 -0400

    readded pool in connection.js

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,12 @@ var Sequelize = require("sequelize");
 var sequelize = new Sequelize("burgers", "root", "", {
   host: "localhost",
   port: 3306,
-  dialect: "mysql"
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
 });
 
 // Exports the connection for other files to use
