13ddfdac8db1edd80a0457882818547c2a3a5070
Austin T <austin.taranto@gmail.com>
2019-07-28 18:01:07 -0400

    changed connection.js code

--- config/connection.js
+++ config/connection.js
@@ -1,40 +1,22 @@
-// Dependencies
-var Sequelize = require("sequelize");
+var mysql = require("mysql");
 
-// Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize("burgers", "root", "vodkamysql1", {
+var connection = mysql.createConnection({
   host: "localhost",
+
+  // Your port; if not 3306
   port: 3306,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "vodkamysql1",
+  database: "burgers_db"
 });
 
-// Exports the connection for other files to use
-module.exports = sequelize;
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected as id " + connection.threadId);
+  });
 
-// var mysql = require("mysql");
-
-// var connection = mysql.createConnection({
-//   host: "localhost",
-
-//   // Your port; if not 3306
-//   port: 3306,
-
-//   // Your username
-//   user: "root",
-
-//   // Your password
-//   password: "vodkamysql1",
-//   database: "burgers_db"
-// });
-
-//   con.connect(function(err) {
-//     if (err) throw err;
-//     console.log("Connected!");
-//   });
-
-//   module.exports = ;
+  module.exports = connection;
